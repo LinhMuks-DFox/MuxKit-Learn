f66c4381c88912876a9232e68898a1ead91c3840
LinhMux <muxxum65535@gmail.com>
2023-01-01 00:05:49 +0900

    adding unit test for ConvNDimNTimes, happy new year!

--- unit_test/convolution/ConvNDimNTime.py
+++ unit_test/convolution/ConvNDimNTime.py
@@ -1,7 +1,13 @@
 import unittest
+from mklearn.convolution.ConvoNTime import ConvNDimNTimes
 
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        raise NotImplementedError("Not implemented yet.")
+        self.conv = ConvNDimNTimes()
+
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
